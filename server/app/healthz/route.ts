e55130cdfac244a5ef51e5f8eb64d5ed98c07e6b
Marks Polakovs <marks@markspolakovs.me>
2023-07-24 17:12:08 +0100

    Fix healthz dynamic build error

--- server/app/healthz/route.ts
+++ server/app/healthz/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   await db.$queryRaw`SELECT 1+1;`;
   return NextResponse.json({ ok: true });
