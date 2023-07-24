cfd474aaf8459769216be5297602dbe3433f3da5
Marks Polakovs <marks@markspolakovs.me>
2023-07-25 00:36:06 +0100

    Fix auth redirects when running in Docker

--- server/middleware.ts
+++ server/middleware.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 export const middleware = auth((req) => {
   if (!req.auth.user) {
     const url = req.nextUrl.clone();
+    // Handle running in Docker
+    if (process.env.PUBLIC_URL) {
+      const publicUrl = new URL(process.env.PUBLIC_URL);
+      url.host = publicUrl.host;
+    }
     url.searchParams.set("callbackUrl", url.href);
     url.pathname = "/auth/signin";
     return NextResponse.redirect(url);
