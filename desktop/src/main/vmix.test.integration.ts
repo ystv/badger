d36fea965b4b2b2e5f61f29433fa9ea0445bd8b0
Marks Polakovs <github@markspolakovs.me>
2023-08-05 12:00:13 +0100

    Couple more "real world" fixes

--- desktop/src/main/vmix.test.integration.ts
+++ desktop/src/main/vmix.test.integration.ts
@@ -11,9 +11,12 @@ integrate("VMixConnection integration", () => {
       console.log("Setup done");
     });
     test("getFullState", async () => {
+      expect.assertions(1);
       // The current state of the vMix instance is unpredictable, so all we can do is test that it doesn't throw
       // (i.e. it can successfully parse the state, whatever it may be).
-      expect(vmix.getFullState()).resolves.toHaveProperty("edition");
+      // Doing this await rather than expect().resolves.toHaveProperty() means we get better error messages.
+      const r = await vmix.getFullState();
+      expect(r).toHaveProperty("edition");
     });
   });
   
