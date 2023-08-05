3f9f6c6272d106d5b679ce322175bf3e9131ee63
Marks Polakovs <github@markspolakovs.me>
2023-08-05 18:27:46 +0100

    vMix UI improvements and fixes

--- desktop/src/renderer/MainScreen.tsx
+++ desktop/src/renderer/MainScreen.tsx
@@ -3,28 +3,67 @@ import invariant from "../common/invariant";
 import { Popover, Tab } from "@headlessui/react";
 import { IoCog, IoDownloadSharp } from "react-icons/io5";
 import OBSScreen from "./screens/OBS";
-import { DownloadTracker } from "./DownloadTracker";
 import VMixScreen from "./screens/vMix";
+import { usePopper } from "react-popper";
+import { useMemo, useState } from "react";
+
+function DownloadTrackerPopup() {
+  const downloadStatus = ipc.media.getDownloadStatus.useQuery(void 0, {
+    refetchInterval: 1000,
+  });
+
+  const [refEl, setRefEl] = useState<HTMLElement | null>(null);
+  const [popoverEl, setPopoverEl] = useState<HTMLElement | null>(null);
+  const { styles, attributes } = usePopper(refEl, popoverEl, {
+    placement: "bottom-end",
+  });
+
+  const downloads = useMemo(
+    () => downloadStatus.data?.filter((x) => x.status !== "done"),
+    [downloadStatus.data],
+  );
+
+  if (!downloads?.length) {
+    return null;
+  }
+
+  return (
+    <Popover>
+      <Popover.Button ref={setRefEl}>
+        <IoDownloadSharp className="h-8 w-8" size={32} />
+      </Popover.Button>
+      <Popover.Panel
+        ref={setPopoverEl}
+        style={styles.popper}
+        {...attributes.popper}
+        className="bg-light text-dark px-2 py-4 shadow-lg"
+      >
+        {downloads.map((download) => (
+          <div key={download.mediaID}>
+            <strong>{download.name}</strong>: {download.status},{" "}
+            {download.progressPercent?.toFixed(1)}%
+          </div>
+        ))}
+      </Popover.Panel>
+    </Popover>
+  );
+}
 
 export default function MainScreen() {
   const { data: show } = ipc.getSelectedShow.useQuery();
   invariant(show, "no selected show"); // this is safe because MainScreen is rendered inside a ConnectAndSelectShowGate
   const [integrations] = ipc.supportedIntegrations.useSuspenseQuery();
+
   return (
     <div>
       <nav className="absolute top-0 left-0 w-full h-12 mb-12 px-4 bg-dark text-light flex flex-nowrap items-center justify-between">
         <span className="font-bold">{show.name}</span>
-        <Popover>
-          <Popover.Button>
-            <IoDownloadSharp />
-          </Popover.Button>
-          <Popover.Panel>
-            <DownloadTracker />
-          </Popover.Panel>
-        </Popover>
-        <button className="h-full aspect-square flex items-center justify-center">
+        <div className="ml-auto flex flex-row flex-nowrap">
+          <DownloadTrackerPopup />
+          {/* <button className="h-full aspect-square flex items-center justify-center">
           <IoCog className="h-8 w-8" size={32} />
-        </button>
+        </button> */}
+        </div>
       </nav>
       <div className="relative mb-12" />
       <Tab.Group>
