9809797b67a8e7f602e3bcad65313b127515f5e9
Marks Polakovs <marks@markspolakovs.me>
2023-08-13 21:31:48 +0100

    Use baseURL

--- server/e2e/auth.setup.ts
+++ server/e2e/auth.setup.ts
@@ -2,9 +2,9 @@ import { test as setup } from "@playwright/test";
 
 const authFile = "playwright/.auth/user.json";
 
-setup("authenticate", async ({ page }) => {
+setup("authenticate", async ({ page, baseURL }) => {
   // Perform authentication steps. Replace these actions with your own.
-  await page.goto("http://localhost:3000/login?return=http://localhost:3000");
+  await page.goto(`/login?return=${baseURL}`);
   await page.getByLabel("Username").fill("test");
   await page.getByLabel("Password").fill("test");
   await page.getByRole("button", { name: "Sign in" }).click();
@@ -12,7 +12,7 @@ setup("authenticate", async ({ page }) => {
   //
   // Sometimes login flow sets cookies in the process of several redirects.
   // Wait for the final URL to ensure that the cookies are actually set.
-  await page.waitForURL("http://localhost:3000");
+  await page.waitForURL(baseURL ?? "http://localhost:3000");
 
   // End of authentication steps.
 
--- server/e2e/shows.spec.ts
+++ server/e2e/shows.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("loads", async ({ page }) => {
-  await page.goto("http://localhost:3000/shows");
+  await page.goto("/shows");
 
   await expect(page.getByRole("heading", { name: "Shows" })).toBeVisible();
 });
 
 test("create show", async ({ page }) => {
-  await page.goto("http://localhost:3000/shows");
+  await page.goto("/shows");
   await page.getByRole("button", { name: "New Show" }).click();
   await page.getByLabel("Name").fill("Test Show");
   await page.getByLabel("Start").click();
