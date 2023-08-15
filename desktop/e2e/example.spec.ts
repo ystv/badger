9c118aa2fcdd9ee984ae1b79d295b1068f44a6f0
Marks Polakovs <marks@markspolakovs.me>
2023-08-15 12:09:53 +0100

    [BOW-70] E2E tests for Desktop

---
+++ desktop/e2e/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, _electron as electron } from "@playwright/test";
+
+test("starts", async () => {
+  const app = await electron.launch({ args: [".vite/build/main.js"] });
+  const window = await app.firstWindow();
+  await window.waitForLoadState("domcontentloaded");
+  await expect(window.getByRole("heading", { name: "Bowser" })).toBeVisible();
+});
+
+test("can connect to server", async () => {
+  const app = await electron.launch({ args: [".vite/build/main.js"] });
+  const window = await app.firstWindow();
+  await window.waitForLoadState("domcontentloaded");
+
+  await window.getByLabel("Server address").fill("http://localhost:3000");
+  await window.getByLabel("Server Password").fill("aaa");
+
+  await window.getByRole("button", { name: "Connect" }).click();
+
+  await expect(
+    window.getByRole("heading", { name: "Select a show" }),
+  ).toBeVisible();
+});
