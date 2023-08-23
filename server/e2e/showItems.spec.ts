41ecfd25e4dc945fd845b034d04067e4ff1311fe
Marks Polakovs <marks@markspolakovs.me>
2023-08-23 18:23:57 +0100

    Improve resiliency of drag-and-drop test

--- server/e2e/showItems.spec.ts
+++ server/e2e/showItems.spec.ts
@@ -12,7 +12,7 @@ const test = base.extend<{ showPage: Page }>({
     await page.keyboard.press("Escape");
     await page.getByRole("button", { name: "Create" }).click();
     await expect(
-      page.getByRole("heading", { name: "Test Show" }),
+      page.getByRole("heading", { name: "Test Show" })
     ).toBeVisible();
 
     await use(page);
@@ -39,12 +39,19 @@ test("add, reorder, remove items", async ({ showPage }) => {
   const table = await showPage.getByTestId("ShowItemsList.itemsTable");
   await expect(table.locator("tr")).toHaveCount(3);
 
+  // Work out how far to drag it
+  const heightOfOneRow = await showPage
+    .getByRole("row")
+    .nth(1)
+    .evaluate((r) => r.clientHeight);
+
   await showPage.getByTestId("dragHandle").nth(0).hover();
   await showPage.mouse.down();
-  await showPage.mouse.move(0, 400, { steps: 10 });
+  // Height of three rows + safety margin
+  await showPage.mouse.move(0, heightOfOneRow * 3 + 100, { steps: 10 });
   await showPage.mouse.up();
   await expect(
-    showPage.getByRole("row").nth(3).getByTestId("RundownRow.name"),
+    showPage.getByRole("row").nth(3).getByTestId("RundownRow.name")
   ).toHaveText("Test 1");
 
   await showPage.getByRole("button", { name: "Delet" }).nth(2).click();
@@ -143,13 +150,13 @@ test("add media", async ({ showPage }) => {
     .dispatchEvent("drop", { dataTransfer });
 
   await expect(
-    showPage.getByRole("button", { name: "Media pending" }),
+    showPage.getByRole("button", { name: "Media pending" })
   ).toBeVisible();
   await expect(
-    showPage.getByRole("button", { name: "Media processing" }),
+    showPage.getByRole("button", { name: "Media processing" })
   ).toBeVisible();
   await expect(
-    showPage.getByRole("button", { name: "Good to go!" }),
+    showPage.getByRole("button", { name: "Good to go!" })
   ).toBeVisible({
     timeout: 30_000,
   });
