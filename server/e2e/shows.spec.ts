9b8a6b15362ecd5a39b33561c0a6142b6d082841
Marks Polakovs <marks@markspolakovs.me>
2023-09-05 01:03:59 +0100

    Much better date picker solution
    <3 @sersorrel

--- server/e2e/shows.spec.ts
+++ server/e2e/shows.spec.ts
@@ -16,10 +16,8 @@ test("create show", async ({ page }) => {
   await page.getByLabel("Name").fill("Test Show");
   await page.getByLabel("Start").click();
   // see comment in showItems.spec.ts for an explanation
-  await page
-    .getByRole("button")
-    .filter({ hasNot: page.locator(".opacity-50"), hasText: "27" })
-    .click();
+  await page.getByLabel("Go to next month").click();
+  await page.getByRole("button", { name: "15" }).click();
   await page.keyboard.press("Escape");
   await page.getByRole("button", { name: "Create" }).click();
   await expect(page.getByRole("heading", { name: "Test Show" })).toBeVisible();
