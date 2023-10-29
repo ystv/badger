a4b5b3e25eaa773c964ad19dd710c53f7c586151
Marks Polakovs <marks@markspolakovs.me>
2023-10-29 13:19:17 +0000

    Work-around for E2E test failures

--- server/app/shows/create/page.tsx
+++ server/app/shows/create/page.tsx
@@ -4,12 +4,20 @@ import { schema } from "./schema";
 import { create } from "./actions";
 import Form from "@/components/Form";
 import { DatePickerField, Field } from "@/components/FormFields";
+import { useRouter } from "next/navigation";
 
 export default function CreateShowPage() {
+  const router = useRouter();
   return (
     <div>
       <h1 className="text-4xl">New Show</h1>
-      <Form action={create} schema={schema}>
+      <Form
+        action={create}
+        schema={schema}
+        onSuccess={(v) => {
+          router.push(`/shows/${v.id}`);
+        }}
+      >
         <Field name="name" label="Name" />
         <DatePickerField
           name="start"
