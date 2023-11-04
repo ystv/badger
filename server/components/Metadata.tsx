a02ab8bfefe620e128b06daa29efa99a5ccf5774
Marks Polakovs <marks@markspolakovs.me>
2023-11-04 22:23:32 +0000

    Rework UI

--- server/components/Metadata.tsx
+++ server/components/Metadata.tsx
@@ -6,32 +6,36 @@ import { Input } from "@bowser/components/input";
 import { Textarea } from "@bowser/components/textarea";
 import { Prisma } from "@bowser/prisma/client";
 import { Metadata, MetadataField } from "@bowser/prisma/client";
-import { useId, useMemo, useState, useTransition } from "react";
+import { useId, useMemo, useReducer, useState, useTransition } from "react";
 import { FormResponse } from "./Form";
-import { IoCheckmarkSharp, IoCloseSharp, IoAddCircle } from "react-icons/io5";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@bowser/components/popover";
+  IoCheckmarkSharp,
+  IoCloseSharp,
+  IoAddCircle,
+  IoChevronDownSharp,
+} from "react-icons/io5";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-} from "@bowser/components/select";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@bowser/components/dropdown-menu";
 import { twMerge } from "tailwind-merge";
+import { expectNever } from "ts-expect";
 
 export interface MetadataWithField extends Metadata {
   field: MetadataField;
+  _temporary?: boolean;
 }
 
 function MetaValue(props: {
   field: MetadataField;
   value: Metadata | null;
   saveChanges: (val: Prisma.InputJsonValue) => Promise<FormResponse>;
+  onCancel?: () => void;
   slim?: boolean;
   hideCancel?: boolean;
+  initialTouched?: boolean;
 }) {
   switch (props.field.type) {
     case "Text":
@@ -55,7 +59,7 @@ function MetaValue(props: {
   // This isn't a Rules-of-Hooks violation because the invariants should never be false
   const [isPending, startTransition] = useTransition();
   const [value, setValue] = useState((props.value?.value as string) ?? ""); // will need to change if we add non-text meta types
-  const [touched, setTouched] = useState(false);
+  const [touched, setTouched] = useState(props.initialTouched);
   const [error, setError] = useState<string | null>(null);
   const id = useId();
 
@@ -98,6 +102,7 @@ function MetaValue(props: {
                 color="danger"
                 size={props.slim ? "icon" : "default"}
                 onClick={() => {
+                  props.onCancel?.();
                   setValue((props.value?.value as string) ?? "");
                   setTouched(false);
                 }}
@@ -131,6 +136,34 @@ function MetaValue(props: {
   );
 }
 
+function tempFieldsReducer(
+  state: MetadataWithField[],
+  action:
+    | { type: "add"; fieldType: MetadataField }
+    | { type: "remove"; id: number },
+): MetadataWithField[] {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        {
+          field: action.fieldType,
+          fieldId: action.fieldType.id,
+          showId: -1,
+          rundownId: -1,
+          id: Math.random(),
+          _temporary: true,
+          value: "",
+        },
+      ];
+    case "remove":
+      return state.filter((x) => x.id !== action.id);
+    default:
+      // @ts-expect-error exhaustive
+      invariant(false, `Unknown tempFieldsReducer action type ${action.type}`);
+  }
+}
+
 export function MetadataFields(props: {
   metadata: MetadataWithField[];
   fields: MetadataField[];
@@ -152,9 +185,9 @@ export function MetadataFields(props: {
   );
   const emptyDefaultFields = emptyFields.filter((f) => f.default);
   const emptyNonDefaultFields = emptyFields.filter((f) => !f.default);
-  const [selectedFieldTypeID, setSelectedFieldTypeID] = useState<number | null>(
-    null,
-  );
+
+  const [tempFields, setTempFields] = useReducer(tempFieldsReducer, []);
+
   return (
     <div className="space-y-2 my-8">
       {props.metadata
@@ -187,56 +220,42 @@ export function MetadataFields(props: {
             slim
           />
         ))}
+      {tempFields.map((field) => (
+        <MetaValue
+          key={field.id}
+          field={field.field}
+          value={field}
+          saveChanges={async (val) => {
+            const r = await props.createMeta(field.field.id, val);
+            if (r.ok) {
+              setTempFields({ type: "remove", id: field.id });
+            }
+            return r;
+          }}
+          onCancel={() => setTempFields({ type: "remove", id: field.id })}
+          initialTouched
+          slim
+        />
+      ))}
       {emptyNonDefaultFields.length > 0 && (
-        <Popover>
-          <PopoverTrigger asChild>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <Button color="ghost">
-              <IoAddCircle className="mr-1" />
-              Add Extra Show Details
+              <IoChevronDownSharp className="mr-1" />
+              Add extra show details
             </Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <div className="bg-light text-dark p-4 rounded">
-              <Select
-                value={selectedFieldTypeID?.toString(10)}
-                onValueChange={(v) => setSelectedFieldTypeID(parseInt(v, 10))}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {emptyNonDefaultFields.map((field) => (
+              <DropdownMenuItem
+                key={field.id}
+                onClick={() => setTempFields({ type: "add", fieldType: field })}
               >
-                <SelectTrigger>
-                  {selectedFieldTypeID
-                    ? emptyNonDefaultFields.find(
-                        (x) => x.id === selectedFieldTypeID,
-                      )!.name
-                    : "Select type"}
-                </SelectTrigger>
-                <SelectContent>
-                  {emptyNonDefaultFields.map((field) => (
-                    <SelectItem key={field.id} value={field.id.toString(10)}>
-                      {field.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              {selectedFieldTypeID !== null && (
-                <MetaValue
-                  field={
-                    emptyNonDefaultFields.find(
-                      (x) => x.id === selectedFieldTypeID,
-                    )!
-                  }
-                  value={null}
-                  saveChanges={async (val) => {
-                    const r = await props.createMeta(selectedFieldTypeID!, val);
-                    if (r.ok) {
-                      setSelectedFieldTypeID(null);
-                    }
-                    return r;
-                  }}
-                  hideCancel
-                />
-              )}
-            </div>
-          </PopoverContent>
-        </Popover>
+                {field.name}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </div>
   );
