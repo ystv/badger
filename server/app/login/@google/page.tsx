9e5ad859d46b4a6a535f2a558c56e76f4d210abb
Marks Polakovs <marks@markspolakovs.me>
2023-11-07 00:24:28 +0000

    Tidying

--- server/app/login/@google/page.tsx
+++ server/app/login/@google/page.tsx
@@ -2,10 +2,25 @@ import { cookies } from "next/headers";
 import { GoogleLoginButton } from "./GoogleLoginButton";
 import { Alert } from "@bowser/components/alert";
 import { SignInResult } from "@/lib/auth";
+import invariant from "@/lib/invariant";
+
+function commaJoin(parts: string[]): string {
+  if (parts.length === 0) {
+    return "";
+  } else if (parts.length === 1) {
+    return parts[0];
+  } else {
+    return `${parts.slice(0, -1).join(", ")} or ${parts[parts.length - 1]}`;
+  }
+}
 
 export default async function GoogleLogin(props: {
   searchParams: { return?: string; status?: string };
 }) {
+  invariant(
+    process.env.GOOGLE_CLIENT_ID,
+    "Google sign in enabled but GOOGLE_CLIENT_ID not set",
+  );
   let message;
   switch (props.searchParams.status) {
     case undefined:
@@ -28,8 +43,8 @@ export default async function GoogleLogin(props: {
       {process.env.USER_AUTO_CREATE_DOMAINS && (
         <p>
           If you have a{" "}
-          {process.env.USER_AUTO_CREATE_DOMAINS.split(", ").join(" , or ")}{" "}
-          account, use it to get access faster.
+          {commaJoin(process.env.USER_AUTO_CREATE_DOMAINS.split(", "))} account,
+          use it to get access faster.
         </p>
       )}
       <GoogleLoginButton
