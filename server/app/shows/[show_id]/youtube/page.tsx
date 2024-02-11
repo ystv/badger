158d1bd8287d7270188a43a851544a430817f99f
Marks Polakovs <marks@markspolakovs.me>
2024-02-11 19:16:35 +0000

    Fix typecheck (again)

--- server/app/shows/[show_id]/youtube/page.tsx
+++ server/app/shows/[show_id]/youtube/page.tsx
@@ -57,8 +57,8 @@ export default async function ShowYouTubePage(props: {
   }
 
   const [titleFieldID, descFieldID] = await Promise.all([
-    getSetting("YouTube", "TitleMetadataID"),
-    getSetting("YouTube", "DescriptionMetadataID"),
+    getSetting("TitleMetadataID"),
+    getSetting("DescriptionMetadataID"),
   ]);
 
   return (
