0555538818be4ed47ff14a77f6479c5e6f981152
Marks Polakovs <marks.polakovs@ystv.co.uk>
2024-03-01 13:26:59 +0000

    [BDGR-117] Direct media download links in Server UI (#227)
    * [BDGR-117] Direct media download links in Server UI
    
    * Small text
    
    * Oops

--- server/components/MediaState.tsx
+++ server/components/MediaState.tsx
@@ -26,6 +26,7 @@ import {
   PopoverTrigger,
 } from "@badger/components/popover";
 import { MediaSelectOrUploadDialog, PastShowsMedia } from "./MediaSelection";
+import Link from "next/link";
 
 export interface CompleteMedia extends Media {
   tasks: MediaProcessingTask[];
@@ -180,6 +181,16 @@ function MediaProcessingState({
           >
             Replace
           </Button>
+          {(media.state === MediaState.Ready ||
+            media.state === MediaState.ReadyWithWarnings) && (
+            <Link
+              href={`/media/download/${media.id}`}
+              target="_blank"
+              className="text-sm"
+            >
+              Download
+            </Link>
+          )}
         </div>
       </PopoverContent>
     </Popover>
