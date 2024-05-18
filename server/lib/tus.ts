dc6c13684516658731f2c146dfb385e8dafa056d
Marks Polakovs <marks@markspolakovs.me>
2024-05-18 17:55:17 +0100

    Rework README and docker-compose for public use
    Split the README into "how to use" and "how to
    contribute" (the latter now being CONTRIBUTING.md).
    
    Also, add an example docker-compose file for using
    the public Docker images.
    
    Finally, adapt the tus endpoint discovery to
    support a different public endpoint.

--- server/lib/tus.ts
+++ server/lib/tus.ts
@@ -3,3 +3,7 @@ import "server-only";
 export function getTusEndpoint(): string {
   return process.env.TUS_ENDPOINT!;
 }
+
+export function getPublicTusEndpoint(): string {
+  return process.env.PUBLIC_TUS_ENDPOINT ?? getTusEndpoint();
+}
