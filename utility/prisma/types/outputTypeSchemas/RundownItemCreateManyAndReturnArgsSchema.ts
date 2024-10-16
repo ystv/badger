07be4f0cfad0aa7e80f6e5266b967406e521c5d5
Marks Polakovs <marks@markspolakovs.me>
2024-10-16 21:57:58 +0100

    Regenerate prisma

---
+++ utility/prisma/types/outputTypeSchemas/RundownItemCreateManyAndReturnArgsSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import type { Prisma } from "../../client";
+import { RundownItemCreateManyInputSchema } from "../inputTypeSchemas/RundownItemCreateManyInputSchema";
+
+export const RundownItemCreateManyAndReturnArgsSchema: z.ZodType<Prisma.RundownItemCreateManyAndReturnArgs> =
+  z
+    .object({
+      data: z.union([
+        RundownItemCreateManyInputSchema,
+        RundownItemCreateManyInputSchema.array(),
+      ]),
+      skipDuplicates: z.boolean().optional(),
+    })
+    .strict();
+
+export default RundownItemCreateManyAndReturnArgsSchema;
---
+++ utility/prisma/types/outputTypeSchemas/UserCreateManyAndReturnArgsSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import type { Prisma } from "../../client";
+import { UserCreateManyInputSchema } from "../inputTypeSchemas/UserCreateManyInputSchema";
+
+export const UserCreateManyAndReturnArgsSchema: z.ZodType<Prisma.UserCreateManyAndReturnArgs> =
+  z
+    .object({
+      data: z.union([
+        UserCreateManyInputSchema,
+        UserCreateManyInputSchema.array(),
+      ]),
+      skipDuplicates: z.boolean().optional(),
+    })
+    .strict();
+
+export default UserCreateManyAndReturnArgsSchema;
